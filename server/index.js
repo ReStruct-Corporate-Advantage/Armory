1bc70de1a8affb039a7ccc7f15abb3776b7e3a07
m0n02hz <mohiit1502@gmail.com>
2021-08-09 01:48:38 +0530

    Updated procfile to run start on client as well

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ global.db = db;
 
 io.on("connection", function(socket) {
     console.log("A user connected");
- 
+
     //Whenever someone disconnects this piece of code executed
     socket.on("disconnect", function () {
        console.log("A user disconnected");
